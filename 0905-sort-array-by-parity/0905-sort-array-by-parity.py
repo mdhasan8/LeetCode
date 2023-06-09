6a5929d04db21f5faeb1ea49678765b1ecbdfe33
Md Easin Hasan <mhasan8@miners.utep.edu>
2023-06-08 19:06:30 -0700

    Time: 91 ms (52.81%), Space: 17.1 MB (26.00%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        ans = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] % 2 ==0:
+                ans.append(nums[i])
+        for i in range(len(nums)):
+            if nums[i] % 2 !=0:
+                ans.append(nums[i])
+        return ans
+        
